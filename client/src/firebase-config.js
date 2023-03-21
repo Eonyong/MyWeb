e34e5d778b30b3ac756ee9df22c5a6bf5b4ef8e8
정언용 <unjoo94@naver.com>
2023-03-22 07:44:28 +0900

    adsense add

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
@@ -17,6 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const firebase = initializeApp(firebaseConfig);
 getAnalytics(firebase);
-export const db = getFirestore(firebase);
+const db = getFirestore(firebase);
+
+export { db };
