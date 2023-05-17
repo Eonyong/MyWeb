8623a9a8116bfe1839883fedd5e0b9a2d6f9f139
정언용 <unjoo94@naver.com>
2023-05-17 23:01:02 +0900

    :bug: : API GET headers add

--- client/src/pages/API.tsx
+++ client/src/pages/API.tsx
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 const APIs = (): any => {
+  const pageNo = 1;
+  const topFinGrpNo = '020000';
+  const auth = process.env.REACT_APP_OPEN_API_ID;
   const Option = {
-    url: `https://finlife.fss.or.kr/finlifeapi/companySearch.json`,
+    url: `https://finlife.fss.or.kr/finlifeapi/companySearch.json?auth=${auth}&topFinGrpNo=${topFinGrpNo}&pageNo=${pageNo}`,
     method: 'GET',
-    data: {
-      auth: process.env.REACT_APP_OPEN_API_ID,
-      topFinGrpNo: '020000',
-      pageNo: '1',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth}`,
+      
     },
   };
   axios(Option)
--- client/src/pages/dashboard/home/HomePage.tsx
+++ client/src/pages/dashboard/home/HomePage.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import APIs from '../../API';
 
 const HomePage = () => {
-  APIs()
-  return <div>apis</div>;
+  APIs();
+  return <div>MainPage</div>;
 };
 
 export default HomePage;
